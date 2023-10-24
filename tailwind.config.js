1a97a203f21dc725b1f50322ed1dc4f321828500
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-24 13:26:28 +0300

    tailwind-started

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  //? kendi theme ayarlarımızı eklemek için bu kısmı ekliyoruz
+  theme: {
+    extend: {
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
+      },
+    },
+  },
+   
+  plugins: [require("tw-elements/dist/plugin")],
+};
